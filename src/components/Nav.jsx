8d6558e1a8c67b4085fa155188b5d66e98b6c398
Matt <sweeney.matt93@gmail.com>
2023-01-13 10:07:06 -0600

    updated contact button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav () {
   return(
     <div className='nav'>
       <Link to ='/'>Home</Link>
-      <Link to ='/contact' >Contact</Link>
+      <a className="email" href="mailto:hello@themattsweeney.com?subject=Hello&body=Hello Matt!">Contact</a>
       <Link to ='/portfolio'>Portfolio</Link>
       <a href="https://docs.google.com/document/d/1FDwhAdkIKd_CgDNU9bTa1UNcIHTv6HKqGn8LYFRKQJM/edit?usp=sharing" target="_blank">Resume</a>
       <a href="https://www.linkedin.com/in/sweeney-matt/" target="_blank">LinkedIn</a>
