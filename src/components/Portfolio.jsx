4ff450d232d6dac5efdd397e376f056d1d2fa51f
Matt <sweeney.matt93@gmail.com>
2023-01-18 10:47:06 -0600

    adjusted portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,16 +12,15 @@ export default function Portfolio () {
   <h1 className="pagetitle">Portfolio</h1>
 
   <div className="portcontainer">
-  <a href='https://github.com/M-Sweeney/Tequila_Mockingbird' target="_blank" className="porttitle">Tequila Mockingbird</a>
-  <img className='portimg' src={Teq}/>
-  <h2 className='portdescription'>Tequila Mockingbird is a cocktail search and exploration tool that allows users to browse and search for drinks by name, alcohol type, ingredient type, and drink type. It is designed to help people discover new drinks and expand their palate. Technologies used in this project include React, Javascript, and CSS, and the website makes use of an API from <a href='https://www.thecocktaildb.com/api.php' target="_blank">(https://www.thecocktaildb.com/api.php)</a>. Users can navigate the website using a top navigation bar and can see details and instructions for making specific drinks by clicking on drink or ingredient cards. <a href='https://github.com/M-Sweeney/Tequila_Mockingbird' target="_blank" className='repo'>Check out the repository here!</a></h2>
+  <a href='https://github.com/DuetSocial/Duet_FE' target="_blank" className="porttitle">Duet</a>
+    <img className='portimg' src={Duet}/>
+    <h2 className='portdescription'>Duet is a social app that allows users to connect with others based on their shared interests in music. It features a feed where users can see comments and threads started by others, a connections tab where users can explore profiles they have liked or discover new connections based on their interests, and genre and artist tabs where users can update their interests. The app is built using Sequelize and Express on the backend and React, JavaScript, CSS, and Tailwind on the frontend. Users can get started by creating an account and following prompts to select their favorite music genres and artists, then exploring the feed and connections tab. The app is inspired by sources such as Last.FM, Twitter, and dating apps like Tinder. <a href='https://github.com/DuetSocial/Duet_FE' target="_blank" className='repo'>Check out the repository here!</a></h2>
   </div>
 
   <div className="portcontainer">
-  <a href='https://github.com/DuetSocial/Duet_FE' target="_blank" className="porttitle">Duet</a>
-    <img className='portimg' src={Duet}/>
-    {/* <img className='portimg' src={Duet2}/> */}
-    <h2 className='portdescription'>Duet is a social app that allows users to connect with others based on their shared interests in music. It features a feed where users can see comments and threads started by others, a connections tab where users can explore profiles they have liked or discover new connections based on their interests, and genre and artist tabs where users can update their interests. The app is built using Sequelize and Express on the backend and React, JavaScript, CSS, and Tailwind on the frontend. Users can get started by creating an account and following prompts to select their favorite music genres and artists, then exploring the feed and connections tab. The app is inspired by sources such as Last.FM, Twitter, and dating apps like Tinder. <a href='https://github.com/DuetSocial/Duet_FE' target="_blank" className='repo'>Check out the repository here!</a></h2>
+  <a href='https://github.com/M-Sweeney/Tequila_Mockingbird' target="_blank" className="porttitle">Tequila Mockingbird</a>
+  <img className='portimg' src={Teq}/>
+  <h2 className='portdescription'>Tequila Mockingbird is a cocktail search and exploration tool that allows users to browse and search for drinks by name, alcohol type, ingredient type, and drink type. It is designed to help people discover new drinks and expand their palate. Technologies used in this project include React, Javascript, and CSS, and the website makes use of an API from <a href='https://www.thecocktaildb.com/api.php' target="_blank">(https://www.thecocktaildb.com/api.php)</a>. Users can navigate the website using a top navigation bar and can see details and instructions for making specific drinks by clicking on drink or ingredient cards. <a href='https://github.com/M-Sweeney/Tequila_Mockingbird' target="_blank" className='repo'>Check out the repository here!</a></h2>
   </div>
 
   <div className="portcontainer">
